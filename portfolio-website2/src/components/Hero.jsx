b7c900bb63890e3d0951aaf379d211368a5288ad
JAMES M <168496401+princelamar509@users.noreply.github.com>
2024-09-29 20:14:55 -0400

    upt

--- portfolio-website2/src/components/Hero.jsx
+++ portfolio-website2/src/components/Hero.jsx
@@ -1,11 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Hero.css";
 import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
 import profilePicture from "../assets/profile-picture.webp";
 import TextToSpeechButton from "./Speech-Button";
-
-
-
 
 function Hero() {
   return (
@@ -21,55 +18,80 @@ function Hero() {
             </h1>
             <h2 className="typewriter" id="typewriter">
               {"Front End Developer"}
-
             </h2>
           </div>
         </div>
 
         <div className="hero-image">
           <img src={profilePicture} alt="Profile" draggable={false} />
-         
         </div>
 
         <TextToSpeechButton />
-    
+
         <div className="introduction-section">
-          <h2 className="intro-heading">Let Me <strong className="connect-text"> Introduce </strong>  Myself</h2>
+          <h2 className="intro-heading">
+            Let Me <strong className="connect-text">Introduce</strong> Myself
+          </h2>
           <p className="intro-text">
-            I’m <strong className="connect-text">Madeus Rene</strong> a self-taught Front-end developer who has spent more time debugging code than I care to admit. Armed with curiosity and coffee, I’ve learned how to turn pixels into interactive experiences that <strong className="connect-text">(hopefully)</strong> don't break the internet!
+            I’m <strong className="connect-text">Madeus Rene</strong>, a
+            self-taught Front-end developer with a passion for creating
+            intuitive and dynamic user experiences. Armed with curiosity and
+            coffee, I turn pixels into seamless interactions that{" "}
+            <strong className="connect-text">(hopefully)</strong> make the web
+            a better place!
           </p>
 
           <div className="intro-avatar">
-            <img src={profilePicture} alt="Avatar" draggable={false} className="avatar-container" />
-            <div class="emoji">😊</div>
+            <img
+              src={profilePicture}
+              alt="Avatar"
+              draggable={false}
+              className="avatar-container"
+            />
+            <div className="emoji">😊</div>
             <div className="emoji">😄</div>
             <div className="emoji">😎</div>
           </div>
-         
+
           <h3 className="social-heading">Find Me Online</h3>
           <p className="social-text">
-            Feel free to <strong className="connect-text">connect</strong> with me.
+            Feel free to <strong className="connect-text">connect</strong> with
+            me.
           </p>
 
           <div className="intro-social-icons">
-            <a href="https://github.com/princelamar509" target="_blank" rel="noopener noreferrer">
-              <FaGithub size={50} />
+            <a
+              href="https://github.com/princelamar509"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub size={40} />
             </a>
-            <a href="https://www.linkedin.com/in/madeus-rene-294196318/" target="_blank" rel="noopener noreferrer">
-              <FaLinkedin size={50} />
+            <a
+              href="https://www.linkedin.com/in/madeus-rene-294196318/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin size={40} />
             </a>
-            <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
-              <FaTwitter size={50} />
+            <a
+              href="https://twitter.com/yourusername"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter size={40} />
             </a>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-              <FaInstagram size={50} />
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram size={40} />
             </a>
           </div>
         </div>
       </div>
     </section>
-
-
   );
 }
 
