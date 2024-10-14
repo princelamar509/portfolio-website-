fbea99875a47ad8917436448455e8e5e5f2ab348
JAMES M <168496401+princelamar509@users.noreply.github.com>
2024-10-13 23:43:27 -0400

    new

--- portfolio-website2/src/components/MarqueeButton.jsx
+++ portfolio-website2/src/components/MarqueeButton.jsx
@@ -5,9 +5,9 @@ import './MarqueeButton.css';
 const MarqueeButton = ({ onClick }) => {
   return (
     <button className="marquee-button" type="button" onClick={onClick}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="90" fill="none" stroke="#000" strokeWidth="5"   viewBox="0 0 100 100">
-        <circle cx="50" cy="50" r="60"  fillOpacity="0" stroke="#DAA520" strokeWidth="6" />
-        <text x="50%" y="50%" textAnchor="middle" dy="0.3em" fontSize="30" fill="#000">Click Me</text>
+      <svg xmlns="http://www.w3.org/2000/svg" width="90" height="70" fill="none" stroke="#000" strokeWidth="1"   viewBox="0 0 100 100">
+        <circle cx="50" cy="50" r="60"  fillOpacity="0" stroke="#DAA520" strokeWidth="3" />
+        <text x="50%" y="50%" textAnchor="middle" dy="0.45rem" fontSize="30" fill="#000">Click Me</text>
       </svg>
     </button>
   );
